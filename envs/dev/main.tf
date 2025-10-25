c7ba12bcaaf959a3c2b9b938b44cb40a311f88d5
ManuelSalcedo2770 <126840967+ManuelSalcedo2770@users.noreply.github.com>
2025-10-25 04:41:14 -0600

    fix: change S3 backend region to us-east-1

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "draftbook-terraform-state-537692431882"
     key     = "dev/terraform.tfstate"
-    region  = "us-east-2"
+    region  = "us-east-1"  # El bucket está en us-east-1
     encrypt = true
   }
 }
